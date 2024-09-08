01ef633ccb0c7d3de15f4945ead817852c85a73d
m-somov <matvey.somov.3141@gmail.com>
2024-09-08 11:02:43 +0500

    feat(lab1): Выполнена лабораторная работа 1

---
+++ lab1/MyFirstTestAppSpringBoot/src/test/java/m/somov/MyFirstTestAppSpringBoot/MyFirstTestAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package m.somov.MyFirstTestAppSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstTestAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
